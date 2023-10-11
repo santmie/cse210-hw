7bdcb51cfed6b2e0919f6a952c99c87f68a3664d
santmie <san.advincula@gmail.com>
2023-10-11 16:45:39 +0800

    completed wk5 Encapsulation learning activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Console.WriteLine("Hello Learning03 World!");
+        Console.WriteLine("Verify that you can create fractions using all three of these constructors.");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+
+        fraction1.Display();
+        fraction2.Display();
+        fraction3.Display();
+
+        Console.WriteLine();
+        Console.WriteLine("Verify that you can call each constructor and that you can retrieve and display the different representations for a few different fractions");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+        
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
     }
 }
