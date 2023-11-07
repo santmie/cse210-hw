2f203a8d81dd0bdded5d60385a24950dd762a7fc
santmie <san.advincula@gmail.com>
2023-11-07 23:20:14 +0800

    completed wk9 Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Yellow", 2);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Orange", 3, 4);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Violet", 5);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = Math.Round(s.GetArea(), 2);
+           
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
+
     }
 }
